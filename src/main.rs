a94d0b86d7904412e1f77a15e3c59a539272417a
coal-rock <colebrazinski15@gmail.com>
2025-02-27 09:46:08 -0500

    holy shit this is so good

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,91 @@ use iocraft::prelude::*;
 #[tokio::main]
 async fn main() -> Result<()> {
     element! {
-        View(
-            border_style: BorderStyle::Round,
-            border_color: Color::Blue,
-            width: Size::Length(100),
-            height: Size::Length(20),
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            display: Display::Flex,
-            flex_direction: FlexDirection::Column,
-        ) {
-            View() {
-                View(
-                    border_style: BorderStyle::Round,
-                    border_color: Color::Red,
-                    justify_content: JustifyContent::Center,
-
-                ) {
-                        Text(content: "Hello, world", align: TextAlign::Right, wrap: TextWrap::NoWrap)
-                }
-            }
-            View() {
-                View(
-                    border_style: BorderStyle::Round,
-                    border_color: Color::Red,
-                    justify_content: JustifyContent::Center,
-
-                ) {
-                        Text(content: "Hello, world", align: TextAlign::Right, wrap: TextWrap::NoWrap)
-                }
-            }
-        }
+        MainPage()
     }
     .fullscreen()
     .await?;
 
     Ok(())
+}
+
+#[component]
+fn MainPage(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    let (width, height) = hooks.use_terminal_size();
+
+    element! {
+        View(
+            display: Display::Flex,
+            height: height,
+            width: width,
+            flex_direction: FlexDirection::Column,
+            padding_left: 1,
+            padding_right: 1,
+        ) {
+            StatusBar()
+
+            View(
+                display: Display::Flex,
+                flex_direction: FlexDirection::Row,
+                height: 100pct,
+                ) {
+                NoteList()
+                NoteContent()
+            }
+
+            SearchBar()
+        }
+    }
+}
+
+#[component]
+fn StatusBar(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            height: 1,
+            padding_left: 1,
+            padding_right: 1,
+            display: Display::Flex,
+            justify_content: JustifyContent::SpaceBetween,
+        ) {
+            Text(content: "Thoughts")
+            Text(content: "Esc to exit")
+            Text(content: "entry count: 144")
+        }
+    }
+}
+
+#[component]
+fn NoteList(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            border_style: BorderStyle::Round,
+            border_color: Color::White,
+            flex_grow: 1.0,
+        )
+    }
+}
+
+#[component]
+fn NoteContent(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            border_style: BorderStyle::Round,
+            border_color: Color::White,
+            flex_grow: 1.0,
+        )
+    }
+}
+
+#[component]
+fn SearchBar(mut hooks: Hooks) -> impl Into<AnyElement<'static>> {
+    element! {
+        View(
+            height: 3,
+            border_style: BorderStyle::Round,
+            border_color: Color::White,
+        ) {
+            Text(content: "Search: ")
+        }
+    }
 }
